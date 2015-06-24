b0cab086f9bf2515baa80d59201af6e76f0abef9
Matthew Saul <matthewsaul22@yahoo.com>
2015-06-23 21:08:34 -0700

    Homework complete for Day 2

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//    $('a').click(function() {
+//        if (".target a").parent().hasClass("active") {
+//        		$('li')addClass("active");
+// 		}
+//    }); 
+// });
+
